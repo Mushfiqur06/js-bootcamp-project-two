8c7494b78b8392321c6c67da12b017003d477ffb
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-09-21 23:32:47 +0600

    Create Helper file

---
+++ src/helper.js
@@ -0,0 +1,50 @@
+export function getID(id) {
+  try {
+    return document.getElementById(id);
+  } catch (err) {
+    console.log(err);
+  }
+}
+export function getClass(classes) {
+  try {
+    return document.getElementsByClassName(classes);
+  } catch (err) {
+    console.log(err);
+  }
+}
+export function queryAll(data) {
+  try {
+    return document.querySelectorAll(data);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export function create() {
+  if (arguments.length === 0) {
+    return document.createElement("div");
+  }
+
+  if (arguments.length === 1 && typeof arguments[0] != "object") {
+    return document.createElement(arguments[0]);
+  }
+
+  var tag = arguments[0];
+  var attr = arguments[1] || arguments[0];
+
+  if (arguments.length === 1 && typeof arguments[0] === "object") {
+    tag = "div";
+  }
+
+  var element = document.createElement(tag);
+
+  for (var i in attr) {
+    element.setAttribute(i, attr[i]);
+  }
+
+  return element;
+}
+
+//create() return div
+//create('li') return li
+//create('li', {class: 'list'}) return li, {class: 'list'}
