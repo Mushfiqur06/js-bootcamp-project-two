9a1e9615f6fa33d8644842e48114df7efa4ad62b
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-09-21 23:32:13 +0600

    Add ContactUL

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
-import Contact from "./contact";
-import Contacts from "./contacts";
+import Contact from "./data/contact";
+import Contacts from "./data/contacts";
+import contactUL from "./view/contact/contactUl";
+import { getID } from "./helper";
 
-let contacts = new Contacts({});
-contacts.createContact(new Contact({}));
-contacts.createContact(new Contact({}));
-contacts.createContact(new Contact({}));
-contacts.createContact(new Contact({}));
-const showContactList = contacts.getContact();
-console.log(showContactList);
+const data = [
+  { name: "Primeasia", phone: "8578495745" },
+  { name: "Primeasia One", phone: "8578495745" },
+  { name: "Primeasia Two", phone: "8578495745" },
+  { name: "Primeasia Three", phone: "8578495745" },
+];
+const root = getID("root");
+let UL = contactUL(data);
+console.log(UL);
+root.append(UL);
+// let contacts = new Contacts({});
+// contacts.createContact(new Contact({}));
+// contacts.createContact(new Contact({}));
+// contacts.createContact(new Contact({}));
+// contacts.createContact(new Contact({}));
+// const showContactList = contacts.getContact();
+// console.log(showContactList);
+
+//MVC -- Model View Controller
+//Model -- working with Data
+//View -- Graphical User Interface
+//Controller -- An Adapter to adapt model and view
+
+//GUI + DATA
