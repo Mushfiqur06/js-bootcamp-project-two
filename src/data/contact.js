229081770d221d6593452563e30b777f57e6c2ad
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-09-26 23:24:11 +0600

    some

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,10 +1,11 @@
 import { v4 as uuidv4 } from "uuid";
 
 class Conctact {
-  constructor({ name = null, phone = null, email = null }) {
+  constructor({ name = null, phone = null, email = null, favourite = false }) {
     this.name = name;
     this.phone = phone;
     this.email = email;
+    this.favourite = favourite;
     this.id = uuidv4();
   }
 
